9239f67287176dc6d2cad47c7d1ee366d9a8817b
Vansh Batham <v4nshbatham@gmail.com>
2025-12-15 11:44:34 +0530

    feat(cart): implemented shopping cart.

--- src/main/java/com/ecommerce/project/model/Address.java
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -6,10 +6,6 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "addresses")
@@ -42,12 +38,12 @@ public class Address {
     private String country;
 
     @NotBlank
-    @Size(min = 6, message = "Pincode must be atleast 6 characters")
+    @Size(min = 5, message = "Pincode must be atleast 5 characters")
     private String pinCode;
 
-    @ToString.Exclude
-    @ManyToMany(mappedBy = "addresses")
-    private List<User> users = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public Address(String street, String buildingName, String city, String pinCode, String state, String country) {
         this.street = street;
