2d3c2ce2d8a544aa29de4c9a2f248e6e18583788
Vansh Batham <v4nshbatham@gmail.com>
2026-01-10 12:03:06 +0530

    feat: add SwaggerConfig for API documentation

--- src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
@@ -75,7 +75,7 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests((auth) ->
                         auth.requestMatchers("/api/auth/**").permitAll()
                                 .requestMatchers("/v3/api-docs/**").permitAll()
-                                .requestMatchers("/swagger/ui/**").permitAll()
+                                .requestMatchers("/swagger-ui/**").permitAll()
                                 .requestMatchers("/h2-console/**").permitAll()
                                 //.requestMatchers("api/public/**").permitAll()
                                 //.requestMatchers("api/admin/**").permitAll()
